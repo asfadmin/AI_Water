d2ad7619dc2281db0fa83a047b73c07e40556021
McKade Sorensen (Douglas) <mckades@gmail.com>
2019-06-04 12:09:58 -0800

    Added Neural Network files.

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Alaska Satellite Facility
+Convolutional Neural Network
+McKade Sorensen (Douglas)
+05/21/2019
+
+main.py, runs the code for AI_Project. The first time the program runs it'll create the files
+needed. After its ran once the dataset folder (with all the SAR images) needs to be extracted into
+AI_Project. asf_cnn.h5 and labels.json both need to be moved there into the AI_Project folder.
+"""
+
+import asf_cnn as cnn
+import img_functions
+
+
+def main():
+    img_functions.create_directories()
+    img_functions.move_incorrect_predictions_back()
+    img_functions.move_data_back()
+    # Setting up SAR data
+    img_functions.sar_data_setup()
+    # The paramerter is the percent of the data that is going to be test data.
+    img_functions.test_training_data_percent(30)
+    # Creating and running the CNN.
+    cnn.cnn()
+    img_functions.move_data_back()
+    img_functions.move_incorrect_predictions_back()
+
+
+if __name__ == '__main__':
+    main()
