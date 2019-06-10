ec7807c4ef9fd48e51f4ae88a4cffc69153a6501
Askaholic <askaholic907@gmail.com>
2019-06-10 12:02:22 -0800

    Blacklist ':' in model names
    There is no way to disambiguate multiple colons. For example 
    `mynet:::hello` could be parsed as `mynet: :hello` or `mynet:: hello` or 
    `mynet ::hello`. We will choose to parse the latter way, which means : 
    can't appear in `network_name` parameter of our test.

--- tests/strategies.py
+++ tests/strategies.py
@@ -6,7 +6,7 @@ import hypothesis.strategies as st
 def model_component():
     return st.text(
         alphabet=st.characters(
-            blacklist_categories=('Cs', ), blacklist_characters=('\n')
+            blacklist_categories=('Cs', ), blacklist_characters=('\n', ':')
         ),
         min_size=1
     )
