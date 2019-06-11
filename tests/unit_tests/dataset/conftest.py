181dba335d807087cbca1d261725bbc715e5acbb
Askaholic <askaholic907@gmail.com>
2019-06-11 14:59:45 -0800

    Patch osgeo.gdal.Open in general

--- tests/unit_tests/dataset/conftest.py
+++ tests/unit_tests/dataset/conftest.py
@@ -9,5 +9,5 @@ def mock_gdal_open(img: np.ndarray) -> mock.Mock:
     mock_Open = mock.Mock()
     mock_Open.return_value.ReadAsArray.return_value = img
 
-    with mock.patch("src.dataset.gdal.Open", mock_Open):
+    with mock.patch("osgeo.gdal.Open", mock_Open):
         yield mock_Open
