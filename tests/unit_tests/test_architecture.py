527256f7fc1172948038a85a990bf4ac88cddbe4
Askaholic <askaholic907@gmail.com>
2019-07-08 14:07:59 -0800

    Don't convert tuples to strings

--- tests/unit_tests/test_architecture.py
+++ tests/unit_tests/test_architecture.py
@@ -7,12 +7,12 @@ from src.model.architecture.masked import create_model_masked
 
 
 def model_final_layer(model):
-    return str(model.layers[-1].output_shape)
+    return model.layers[-1].output_shape
 
 
 def test_architecture():
     model = create_model_binary("binary_test")
-    assert model_final_layer(model) == '(None, 1)'
+    assert model_final_layer(model) == (None, 1)
 
     model = create_model_masked("masked_test")
-    assert model_final_layer(model) == '(None, 512, 512, 1)'
+    assert model_final_layer(model) == (None, 512, 512, 1)
