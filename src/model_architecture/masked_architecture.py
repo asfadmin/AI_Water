700c4f4d591d7dddbfc8231b77a37f9da980edf8
McKade Sorensen (Douglas) <mckades@gmail.com>
2019-07-08 15:51:08 -0800

    Add layers to architecture.

--- src/model_architecture/masked_architecture.py
+++ src/model_architecture/masked_architecture.py
@@ -34,7 +34,7 @@ def create_model_masked(model_name, n_filters=16, dropout=0.1, batchnorm=True):
     """Function to define the UNET Model"""
 
     inputs = Input(shape=(512, 512, 1))
-    # Contracting Path
+
     c1 = conv2d_block(inputs, n_filters * 1, kernel_size=3,
                       batchnorm=batchnorm)
     p1 = MaxPooling2D((2, 2))(c1)
@@ -52,35 +52,56 @@ def create_model_masked(model_name, n_filters=16, dropout=0.1, batchnorm=True):
     p4 = MaxPooling2D((2, 2))(c4)
     p4 = Dropout(dropout)(p4)
 
-    c5 = conv2d_block(p4, n_filters=n_filters * 16, kernel_size=3,
+    c4_5 = conv2d_block(p4, n_filters * 8, kernel_size=3, batchnorm=batchnorm)
+    p4_5 = MaxPooling2D((2, 2))(c4_5)
+    p4_5 = Dropout(dropout)(p4_5)
+
+    c4_6 = conv2d_block(p4_5, n_filters * 8, kernel_size=3,
+                        batchnorm=batchnorm)
+    p4_6 = MaxPooling2D((2, 2))(c4_6)
+    p4_6 = Dropout(dropout)(p4_6)
+
+    c5 = conv2d_block(p4_6, n_filters=n_filters * 16, kernel_size=3,
                       batchnorm=batchnorm)
 
     # Expansive Path
     u6 = Conv2DTranspose(n_filters * 8, (3, 3), strides=(2, 2),
                          padding='same')(c5)
-    u6 = concatenate([u6, c4])
+    u6 = concatenate([u6, c4_6])
     u6 = Dropout(dropout)(u6)
     c6 = conv2d_block(u6, n_filters * 8, kernel_size=3, batchnorm=batchnorm)
 
     u7 = Conv2DTranspose(n_filters * 4, (3, 3), strides=(2, 2),
                          padding='same')(c6)
-    u7 = concatenate([u7, c3])
+    u7 = concatenate([u7, c4_5])
     u7 = Dropout(dropout)(u7)
     c7 = conv2d_block(u7, n_filters * 4, kernel_size=3, batchnorm=batchnorm)
 
     u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2),
                          padding='same')(c7)
-    u8 = concatenate([u8, c2])
+    u8 = concatenate([u8, c4])
     u8 = Dropout(dropout)(u8)
     c8 = conv2d_block(u8, n_filters * 2, kernel_size=3, batchnorm=batchnorm)
 
     u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2),
                          padding='same')(c8)
-    u9 = concatenate([u9, c1])
+    u9 = concatenate([u9, c3])
     u9 = Dropout(dropout)(u9)
     c9 = conv2d_block(u9, n_filters * 1, kernel_size=3, batchnorm=batchnorm)
 
-    outputs = Conv2D(1, (1, 1), activation='sigmoid', name='last_layer')(c9)
+    u10 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2),
+                          padding='same')(c9)
+    u10 = concatenate([u10, c2])
+    u10 = Dropout(dropout)(u10)
+    c10 = conv2d_block(u10, n_filters * 1, kernel_size=3, batchnorm=batchnorm)
+
+    u11 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2),
+                          padding='same')(c10)
+    u11 = concatenate([u11, c1])
+    u11 = Dropout(dropout)(u11)
+    c11 = conv2d_block(u11, n_filters * 1, kernel_size=3, batchnorm=batchnorm)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid', name='last_layer')(c11)
     model = Model(inputs=inputs, outputs=[outputs])
 
     model.__asf_model_name = model_name
