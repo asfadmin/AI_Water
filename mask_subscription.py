6c1a45913cd1bcf279d0d4ed4fbc79bbcaf2c560
McKade Sorensen (Douglas) <mckades@gmail.com>
2019-10-04 07:21:59 -0800

    Fixed memory leak

--- mask_subscription.py
+++ mask_subscription.py
@@ -10,11 +10,11 @@ import re
 import shutil
 import time
 from argparse import ArgumentParser, Namespace
+from subprocess import call
 from typing import List
 from zipfile import ZipFile
 
 from asf_hyp3 import API
-from create_mask import main as mask_product
 from scripts.make_vrt import main as vrt
 from src.api_functions import download_prouducts, grab_subscription, hyp3_login
 
@@ -73,7 +73,8 @@ def mask_products(products: List, users_path: str, model_path: str) -> None:
             vv_img = img
 
         output = os.path.join(users_path, f"{folder[0]}_{i}.tif")
-        mask_product(model_path, vv_img, vh_img, output)
+        # Creating mask
+        call(f"python create_mask.py {model_path} {vv_img} {vh_img} {output}".split())
         shutil.rmtree(folder[0])
         os.remove(f"{folder[0]}.zip")
 
