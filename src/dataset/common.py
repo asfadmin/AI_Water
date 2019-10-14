81aced0c2cb7bdf164952cab9b30cf8162c01978
mckadesorensen <dmsorensen@pop-os.localdomain>
2019-10-14 00:55:35 -0800

    Moved check_dependencies into common.py

--- src/dataset/common.py
+++ src/dataset/common.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from ..config import DATASETS_DIR
 from ..model import ModelType
+from typing import Tuple
 
 
 def dataset_dir(dataset: str) -> str:
@@ -27,3 +28,16 @@ def dataset_type(dataset: str) -> ModelType:
     if os.path.isfile(os.path.join(dataset_dir(dataset), 'labels.json')):
         return ModelType.BINARY
     return ModelType.MASKED
+
+
+def check_dependencies(deps: Tuple[str, ...]) -> bool:
+    global_vars = globals()
+
+    for dep in deps:
+        if dep not in global_vars:
+            print(
+                f"This function requires {dep}. "
+                "Please install it in the current shell and try again."
+            )
+            return False
+    return True
