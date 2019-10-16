e3200d148bef0c88216c441cc21f425ab356782a
mckadesorensen <dmsorensen@pop-os.localdomain>
2019-10-16 11:36:52 -0800

    Cleaned up code

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -46,8 +46,11 @@ except ImportError:
 EXT = "tiff|tif|TIFF|TIF"
 FILENAME_REGEX = re.compile(f'.*_ulx_.*\\.(?:{EXT})')
 
+
 # TOD): Bring this back to what it was
-def make_tiles(ifname: str, tile_size: Tuple[int, int], folder='prep_tiles') -> None:
+def make_tiles(ifname: str,
+               tile_size: Tuple[int, int],
+               folder='prep_tiles') -> None:
     """ Takes a .tiff file and breaks it into smaller .tiff files. """
     if folder == "prep_tiles":
         img_fpath = os.path.join(config.PROJECT_DIR, folder, ifname)
