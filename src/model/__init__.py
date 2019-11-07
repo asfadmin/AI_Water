a451a0db627707881873c31d081c3960719df249
mckadesorensen <dmsorensen@pop-os.localdomain>
2019-11-07 13:38:36 -0900

    Updated dem for input/output

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -111,7 +111,7 @@ def load_history_from_path(model_dir: str) -> History:
 
 
 def model_type(model: Model) -> Optional[ModelType]:
-    if model.output_shape == (None, 512, 512, 1):
+    if model.output_shape == (None, 64, 64, 1):
         return ModelType.MASKED
 
     return None
