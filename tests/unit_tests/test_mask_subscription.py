9c53ed96a1828d1e64c5f496b45b7aeac6a0d376
Jason Herning <jmherning93@gmail.com>
2020-02-06 16:24:35 -0900

    regex now matches full file name and uses groups

--- tests/unit_tests/test_mask_subscription.py
+++ tests/unit_tests/test_mask_subscription.py
@@ -1,10 +1,23 @@
 
 import pytest
 import re
+from datetime import datetime
 
 
 
-PRODUCT_REGEX = re.compile(r'(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})_(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})')
+# PRODUCT_REGEX = re.compile(r'(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})_(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})')
+# PRODUCT_REGEX = re.compile(r"""S.*1SDV_(?P<start_year>\d{4})(?P<start_month>\d{2})(?P<start_day>\d{2})T
+#                                         (?P<start_hour>\d{2})(?P<start_minute>\d{2})(?P<start_second>\d{2})_
+#                                         (?P<end_year>\d{4})(?P<end_month>\d{2})(?P<end_day>\d{2})T
+#                                         (?P<end_hour>\d{2})(?P<end_minute>\d{2})(?P<end_second>\d{2})_[0-9]*_.*.zip""")
+
+PRODUCT_REGEX = re.compile(r'S.*1SDV_(?P<start_year>\d{4})(?P<start_month>\d{2})(?P<start_day>\d{2})T(?P<start_hour>\d{2})(?P<start_minute>\d{2})(?P<start_second>\d{2})_(?P<end_year>\d{4})(?P<end_month>\d{2})(?P<end_day>\d{2})T(?P<end_hour>\d{2})(?P<end_minute>\d{2})(?P<end_second>\d{2})_[0-9]*_.*.zip')
+
+def product_datetime(product_name):
+    """takes in product time; uses regex to take out the date/time of the file name
+       then returns a date time object"""
+
+
 
 
 def test_regex():
@@ -22,10 +35,15 @@ def test_regex():
 
 p1 = "S1A_IW_GRDH_1SDV_20190903T023801_20190903T023826_028851_0344EE_3AF9-RESORB-30m-amp-rtc-gamma.zip"
 
-m = re.search(PRODUCT_REGEX, p1)
+m = re.match(PRODUCT_REGEX, p1)
 
-print(m)
-print(m.groups())
+
+
+print(m.groupdict())
+# print(m.groups())
+
+
+# print(PRODUCT_REGEX.groups(p1))
 
 
 
