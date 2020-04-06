734c7d1aa488648946a8346c9389ba00c296317e
Jason Herning <jmherning93@gmail.com>
2020-04-06 10:03:06 -0800

    pep8

--- tests/unit_tests/test_mask_difference.py
+++ tests/unit_tests/test_mask_difference.py
@@ -5,23 +5,37 @@
  unit test for mask_difference.py
 """
 import numpy as np
+import pytest
 
 from scripts.mask_difference import difference
 
+# test bank
 
-def test_difference():
-    x = np.array([[0, 0, 1, 1],
-                  [1, 0, 0, 0],
-                  [1, 1, 1, 0]])
+# Test 1
+test_1_lin = np.array([[0, 0, 1, 1],
+                       [1, 0, 0, 0],
+                       [1, 1, 1, 0]])
 
-    y = np.array([[0, 0, 0, 0],
-                  [1, 1, 1, 1],
-                  [1, 1, 1, 1]])
+test_1_rin = np.array([[0, 0, 0, 0],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
 
-    t1 = difference(x, y)
+test_1_out = np.array([[0, 0, 2, 2],
+                       [0, 1, 1, 1],
+                       [0, 0, 0, 1]])
 
-    out = np.array([[0, 0, 2, 2],
-                    [0, 1, 1, 1],
-                    [0, 0, 0, 1]])
+# Test 2
+test_2_lin = np.array([[0, 0],
+                       [0, 0]])
 
-    assert np.array_equal(t1, out), "Test failed"
+test_2_rin = np.array([[1, 1],
+                       [1, 1]])
+
+test_2_out = np.array([[1, 1],
+                       [1, 1]])
+
+
+@pytest.mark.parametrize("diff, out", [(difference(test_1_lin, test_1_rin), test_1_out),
+                                       (difference(test_2_lin, test_2_rin), test_2_out)])
+def test_mask_difference(diff, out):
+    assert np.array_equal(diff, out), "Test failed"
