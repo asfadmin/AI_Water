09c77d1d4daaeaa024faac3f1c9801930bc0f67d
jmherning <jmherning93@gmail.com>
2020-04-10 18:59:24 -0800

    refactoring to pep8

--- scripts/mask_difference.py
+++ scripts/mask_difference.py
@@ -16,49 +16,50 @@ def intersection(raster1, raster2):
     """Finds the intersection of 2 raster"""
     band1 = raster1.GetRasterBand(1)
     band2 = raster2.GetRasterBand(1)
-    geotransform1 = raster1.GetGeoTransform()
-    geotransform2 = raster2.GetGeoTransform()
+    transform1 = raster1.GetGeoTransform()
+    transform2 = raster2.GetGeoTransform()
 
     # find each image's bounding box
-    # r1 has left, top, right, bottom of dataset's bounds in geospatial coordinates.
-    r1 = [geotransform1[0], geotransform1[3], geotransform1[0] + (geotransform1[1] * raster1.RasterXSize), geotransform1[3] + (geotransform1[5] * raster1.RasterYSize)]
-    r2 = [geotransform2[0], geotransform2[3], geotransform2[0] + (geotransform2[1] * raster2.RasterXSize), geotransform2[3] + (geotransform2[5] * raster2.RasterYSize)]
+    bounds1 = [transform1[0], transform1[3], transform1[0] + (transform1[1] * raster1.RasterXSize),
+               transform1[3] + (transform1[5] * raster1.RasterYSize)]
+    bounds2 = [transform2[0], transform2[3], transform2[0] + (transform2[1] * raster2.RasterXSize),
+               transform2[3] + (transform2[5] * raster2.RasterYSize)]
 
-    # find intersection between bounding boxes
-    intersection = [max(r1[0], r2[0]), min(r1[1], r2[1]), min(r1[2], r2[2]), max(r1[3], r2[3])]
-    if r1 != r2:
+    intersection_between_bounding_boxes = [max(bounds1[0], bounds2[0]), min(bounds1[1], bounds2[1]),
+                                           min(bounds1[2], bounds2[2]), max(bounds1[3], bounds2[3])]
+    if bounds1 != bounds2:
         print("different bounding boxes!")
         # check for any overlap at all...
-        if (intersection[2] < intersection[0]) or (intersection[1] < intersection[3]):
-            intersection = None
+        if (intersection_between_bounding_boxes[2] < intersection_between_bounding_boxes[0]) or (
+                intersection_between_bounding_boxes[1] < intersection_between_bounding_boxes[3]):
             print("no overlap")
             return
         else:
-            print(f"intersection: {intersection}")
+            print(f"intersection: {intersection_between_bounding_boxes}")
 
-            left1 = int(round((intersection[0] - r1[0]) / geotransform1[1]))
-            top1 = int(round((intersection[1] - r1[1]) / geotransform1[5]))
-            col1 = int(round((intersection[2] - r1[0]) / geotransform1[1])) - left1
-            row1 = int(round((intersection[3] - r1[1]) / geotransform1[5])) - top1
+            left1 = int(round((intersection_between_bounding_boxes[0] - bounds1[0]) / transform1[1]))
+            top1 = int(round((intersection_between_bounding_boxes[1] - bounds1[1]) / transform1[5]))
+            col1 = int(round((intersection_between_bounding_boxes[2] - bounds1[0]) / transform1[1])) - left1
+            row1 = int(round((intersection_between_bounding_boxes[3] - bounds1[1]) / transform1[5])) - top1
 
-            left2 = int(round((intersection[0] - r2[0]) / geotransform2[1]))
-            top2 = int(round((intersection[1] - r2[1]) / geotransform2[5]))
-            col2 = int(round((intersection[2] - r2[0]) / geotransform2[1])) - left2
-            row2 = int(round((intersection[3] - r2[1]) / geotransform2[5])) - top2
+            left2 = int(round((intersection_between_bounding_boxes[0] - bounds2[0]) / transform2[1]))
+            top2 = int(round((intersection_between_bounding_boxes[1] - bounds2[1]) / transform2[5]))
+            col2 = int(round((intersection_between_bounding_boxes[2] - bounds2[0]) / transform2[1])) - left2
+            row2 = int(round((intersection_between_bounding_boxes[3] - bounds2[1]) / transform2[5])) - top2
 
             if col1 != col2 or row1 != row2:
                 print("ERROR: COLS and ROWS DO NOT MATCH")
-            # these arrays should now have the same spatial geometry though NaNs may differ
+
             array1 = band1.ReadAsArray(left1, top1, col1, row1)
             array2 = band2.ReadAsArray(left2, top2, col2, row2)
 
-    else:  # same dimensions from the get go
-        col1 = raster1.RasterXSize  # = col2
-        row1 = raster1.RasterYSize  # = row2
+    else:
+        col1 = raster1.RasterXSize
+        row1 = raster1.RasterYSize
         array1 = band1.ReadAsArray()
         array2 = band2.ReadAsArray()
 
-    return array1, array2, col1, row1, intersection
+    return array1, array2, col1, row1, intersection_between_bounding_boxes
 
 
 def get_mask_array(mask_file: str) -> np.ndarray:
@@ -84,20 +85,20 @@ def difference(first_mask: np.ndarray, second_mask: np.ndarray) -> np.ndarray:
 
 
 def create_mask(args: Namespace) -> None:
-    mask1_ds = gdal.Open(args.first_mask)
-    mask2_ds = gdal.Open(args.second_mask)
+    mask1 = gdal.Open(args.first_mask)
+    mask2 = gdal.Open(args.second_mask)
 
-    mask1_intersect, mask2_intersect, col, row, box_intersect = intersection(mask1_ds, mask2_ds)
+    mask1_intersect, mask2_intersect, col, row, box_intersect = intersection(mask1, mask2)
 
     mask_difference = difference(mask1_intersect, mask2_intersect)
 
-    mask_difference_ds = gdal.GetDriverByName('GTiff').Create(args.name, col, row, bands=1)
-    mask_difference_ds.SetGeoTransform((box_intersect[0], mask1_ds.GetGeoTransform()[1], 0, box_intersect[1], 0,
-                                        mask1_ds.GetGeoTransform()[5]))
-    mask_difference_ds.SetProjection(mask1_ds.GetProjection())
+    mask_difference_driver = gdal.GetDriverByName('GTiff').Create(args.name, col, row, bands=1)
+    mask_difference_driver.SetGeoTransform((box_intersect[0], mask1.GetGeoTransform()[1], 0, box_intersect[1], 0,
+                                            mask1.GetGeoTransform()[5]))
+    mask_difference_driver.SetProjection(mask1.GetProjection())
 
-    mask_difference_ds.GetRasterBand(1).WriteArray(mask_difference)
-    mask_difference_ds.GetRasterBand(1).SetNoDataValue(0)
+    mask_difference_driver.GetRasterBand(1).WriteArray(mask_difference)
+    mask_difference_driver.GetRasterBand(1).SetNoDataValue(0)
 
 
 if __name__ == '__main__':
