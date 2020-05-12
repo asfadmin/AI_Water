d45e457d445c0bda27b455adc3c2def761d2ddd5
jmherning <jmherning93@gmail.com>
2020-05-12 05:22:09 -0800

    added get_bounds()

--- scripts/mask_difference.py
+++ scripts/mask_difference.py
@@ -13,7 +13,15 @@ from osgeo import gdal
 from scripts.mask_shape import raster_boundary2shape
 
 
-#TODO: add expected output
+def get_bounds(raster):
+    left, pixel_width, _, top, _, pixel_height = raster.GetGeoTransform()
+    right = left + (raster.RasterXSize * pixel_width)
+    bottom = top + (raster.RasterYSize * pixel_height)
+    return left, top, right, bottom
+
+
+# TODO: add expected output
+# TODO: move to sperate "tools/utility" file
 def intersection(raster1, raster2):
     """Finds the intersection of 2 raster"""
     band1 = raster1.GetRasterBand(1)
@@ -22,50 +30,37 @@ def intersection(raster1, raster2):
     transform2 = raster2.GetGeoTransform()
 
     # find each image's bounding box
-    bounds1 = [transform1[0], transform1[3], transform1[0] + (transform1[1] * raster1.RasterXSize),
-               transform1[3] + (transform1[5] * raster1.RasterYSize)]
-    bounds2 = [transform2[0], transform2[3], transform2[0] + (transform2[1] * raster2.RasterXSize),
-               transform2[3] + (transform2[5] * raster2.RasterYSize)]
+    bounds1 = get_bounds(raster1)
+    bounds2 = get_bounds(raster2)
 
-    intersection_between_bounding_boxes = [max(bounds1[0], bounds2[0]), min(bounds1[1], bounds2[1]),
-                                           min(bounds1[2], bounds2[2]), max(bounds1[3], bounds2[3])]
-    if bounds1 != bounds2:
-        print("different bounding boxes!")
-        # check for any overlap at all...
-        if (intersection_between_bounding_boxes[2] < intersection_between_bounding_boxes[0]) or (
-                intersection_between_bounding_boxes[1] < intersection_between_bounding_boxes[3]):
-            print("no overlap")
-            return
-        else:
-            print(f"intersection: {intersection_between_bounding_boxes}")
+    intersection = [max(bounds1[0], bounds2[0]), min(bounds1[1], bounds2[1]),
+                    min(bounds1[2], bounds2[2]), max(bounds1[3], bounds2[3])]
 
-            left1 = int(round((intersection_between_bounding_boxes[0] - bounds1[0]) / transform1[1]))
-            top1 = int(round((intersection_between_bounding_boxes[1] - bounds1[1]) / transform1[5]))
-            col1 = int(round((intersection_between_bounding_boxes[2] - bounds1[0]) / transform1[1])) - left1
-            row1 = int(round((intersection_between_bounding_boxes[3] - bounds1[1]) / transform1[5])) - top1
+    if (intersection[2] < intersection[0]) or (intersection[1] < intersection[3]):
+        intersection = None
 
-            left2 = int(round((intersection_between_bounding_boxes[0] - bounds2[0]) / transform2[1]))
-            top2 = int(round((intersection_between_bounding_boxes[1] - bounds2[1]) / transform2[5]))
-            col2 = int(round((intersection_between_bounding_boxes[2] - bounds2[0]) / transform2[1])) - left2
-            row2 = int(round((intersection_between_bounding_boxes[3] - bounds2[1]) / transform2[5])) - top2
+    left1 = int(round((intersection[0] - bounds1[0]) / transform1[1]))
+    top1 = int(round((intersection[1] - bounds1[1]) / transform1[5]))
+    col1 = int(round((intersection[2] - bounds1[0]) / transform1[1])) - left1
+    row1 = int(round((intersection[3] - bounds1[1]) / transform1[5])) - top1
 
-            if col1 != col2 or row1 != row2:
-                print("ERROR: COLS and ROWS DO NOT MATCH")
+    left2 = int(round((intersection[0] - bounds2[0]) / transform2[1]))
+    top2 = int(round((intersection[1] - bounds2[1]) / transform2[5]))
+    col2 = int(round((intersection[2] - bounds2[0]) / transform2[1])) - left2
+    row2 = int(round((intersection[3] - bounds2[1]) / transform2[5])) - top2
 
-            array1 = band1.ReadAsArray(left1, top1, col1, row1)
-            array2 = band2.ReadAsArray(left2, top2, col2, row2)
+    array1 = band1.ReadAsArray(left1, top1, col1, row1)
+    array2 = band2.ReadAsArray(left2, top2, col2, row2)
 
-    else:
-        col1 = raster1.RasterXSize
-        row1 = raster1.RasterYSize
-        array1 = band1.ReadAsArray()
-        array2 = band2.ReadAsArray()
-
-    return array1, array2, col1, row1, intersection_between_bounding_boxes
+    return array1, array2, col1, row1, intersection
 
 
 
-#TODO: function is not being used. Find a purpose for it or remove it!
+
+
+
+
+# TODO: function is not being used. Find a purpose for it or remove it!
 def get_mask_array(mask_file: str) -> np.ndarray:
     """Input mask TIFF file, returns nd.array"""
     f = gdal.Open(mask_file)
