84bc1c57cf22884f1ef1c546c65336e9e73c0f08
jmherning <jmherning93@gmail.com>
2020-07-13 21:51:40 -0800

    unit test for VV_REGEX and VH_REGEX

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -2,8 +2,35 @@
  Created By:   Jason Herning
  Date Started: 07-08-2020
  File Name:    test_config.py
- Description:  unit test for make_data.py
+ Description:  unit test for config.py
 """
 
 import numpy as np
+import re
 import pytest
+
+from src.config import VV_REGEX, VH_REGEX
+
+
+# tests for VV_REGEX()
+@pytest.mark.parametrize("input_string, expected_bool", [("s1b_iw_rt30_20190624t224459_g_gpn_83.vv.tif", True),
+                                                         ("s1b_iw_rt30_20190624t224459_g_gpn_83.vh.tif", False),
+                                                         (".tif", False),
+                                                         ("abc", False),
+                                                         ("s_12345_.tif", False)])
+def test_VV_REGEX(input_string, expected_bool):
+    input = bool(re.search(VV_REGEX, input_string))
+    expected = expected_bool
+    assert input == expected, f"regex {input} does NOT match {expected}"
+
+
+# tests for VH_REGEX()
+@pytest.mark.parametrize("input_string, expected_bool", [("s1b_iw_rt30_20190624t224459_g_gpn_83.vh.tif", True),
+                                                         ("s1b_iw_rt30_20190624t224459_g_gpn_83.vv.tif", False),
+                                                         (".tif", False),
+                                                         ("abc", False),
+                                                         ("s_12345_.tif", False)])
+def test_VH_REGEX(input_string, expected_bool):
+    input = bool(re.search(VH_REGEX, input_string))
+    expected = expected_bool
+    assert input == expected, f"regex {input} does NOT match {expected}"
