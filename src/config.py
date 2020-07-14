a39bab246981f7cd1040f7dcbb0a29f17f68ab19
jmherning <jmherning93@gmail.com>
2020-07-13 21:50:44 -0800

    migrating globaly usefell regex to config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,21 @@
+"""
+ Created By:   ?
+ Date Started: ?
+ Last Updated: 07-13-2020
+ File Name:    config.py
+ Description:  Constants to be imported by other files.
+"""
+
+
 import os
+import re
 
 PROJECT_DIR = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 DATASETS_DIR = os.path.join(PROJECT_DIR, 'datasets')
 MODELS_DIR = os.path.join(PROJECT_DIR, 'models')
 NETWORK_DEMS = 512
+
+VH_REGEX = re.compile(r'(.*)_([0-9]+).vh.tif')
+VV_REGEX = re.compile(r'(.*)_([0-9]+).vv.tif')
+
+
---
+++ tests/unit_tests/test_config.py
@@ -0,0 +1,9 @@
+"""
+ Created By:   Jason Herning
+ Date Started: 07-08-2020
+ File Name:    test_config.py
+ Description:  unit test for make_data.py
+"""
+
+import numpy as np
+import pytest
