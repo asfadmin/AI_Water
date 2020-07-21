c8aa3b618fdaa077dfca1e8c9533734768a00d01
jmherning <jmherning93@gmail.com>
2020-07-21 11:44:26 -0800

    added unit test for some regex

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -5,10 +5,8 @@
  Description:  unit test for config.py
 """
 
-import numpy as np
 import re
 import pytest
-
 from src.config import VV_REGEX, VH_REGEX, ZIP_REGEX
 
 
@@ -36,7 +34,7 @@ def test_VH_REGEX(input_string, expected_bool):
     assert input == expected, f"regex {input} does NOT match {expected}"
 
 
-# tests for VH_REGEX()
+# tests for ZIP_REGEX()
 @pytest.mark.parametrize("input_string, expected_bool", [
     ("S1A_IW_GRDH_1SDV_20190919T001902_20190919T001927_029083_034D00_5E45-PREDORB-30m-amp-filt-rtc-gamma.zip", True),
     ("S1A_IW_GRDH_1SDV_20200622T211736_20200622T211801_033135_03D6B2_C54A-RESORB_30m_nomatch_power_gamma0-rtc-gamma.zip", True),
