ce4baccf1a3db986cc61ff2c372e3ac66e4176c7
jmherning <jmherning93@gmail.com>
2020-10-02 05:54:17 -0800

    added create mask script to click interface in aiwater

--- aiwater.py
+++ aiwater.py
@@ -7,40 +7,95 @@
  Description:  Main file holding all the argparse
 """
 
-from argparse import Namespace, ArgumentParser
-import sys
+import click
+from pathlib import Path
+from src.product_download_api import download_metalink_products
+import src.geo_utility as gu
 
 
-def hello(args: Namespace):
-    x = f"hello {args.name}!" * args.count
-    print(x)
-    return x
+@click.group()
+def cli():
+    pass
 
 
+# TODO: Add default output_directory and make option.
+# TODO: Add loading bar.
+@cli.command()
+@click.argument('metalink_path')
+@click.argument('output_directory')
+def download_metalink(metalink_path, output_directory):
+    """Download files from products.metalink"""
+    download_metalink_products(Path(metalink_path), Path(output_directory))
 
-def cmdline_args():
-    # Make parser object
-    p = ArgumentParser(description=__doc__)
 
-    p.add_argument('name', help="your name")
-    p.add_argument("count", type=int, help="number of times to list name")
-    p.add_subparsers()
-    p.set_defaults(func=hello)
+# TODO: create default file name as granule name with _MASK or _mask appended.
+# TODO: make default save directory be the water mask directory
+# TODO: Make it so outfile by default looks in mask directory, and if not there checks it as a path in current dir.
+@cli.command()
+@click.argument('model_path')
+@click.argument('vv_path')
+@click.argument('vh_path')
+@click.argument('outfile')
+@click.option('-v', '--verbose', help="keras verbosity",default=1, type=int)
+def create_mask(model_path, vv_path, vh_path, outfile, verbose):
+    """Create a water mask for an image.
+    The image must be dual pol (VV + VH) and must be calibrated. ****** NOTE ******
+    create_mask.py contains a memory leak. Use a subprocess call when using main in a loop to prevent memory issues.
 
-    return p.parse_args()
+    \b
+    MODEL_PATH Name of the trained model
+    VV_PATH    Path to the calibrated VV tiff
+    VH_PATH    Path to the calibrated VH tiff
+    OUTFILE    Name of the generated mask
+    """
+    gu.create_water_mask(model_path, vv_path, vh_path, outfile, verbose)
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+
+# SCRIPTS TO ADD/CREATE
+
+# TODO: Combine with download_metalink
+"""
+    Options/flags to add:
+        - date/time
+        - frame
+        - path 
+"""
+@cli.command()
+def download_subscription():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+
+@cli.command()
+def train():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+@cli.command()
+def test():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+# tiles the dataset
+@cli.command()
+def create_dataset():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+# use matplot lib to groom the mask images
+@cli.command()
+def groom_dataset():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+
+# print stats found in info_model
+@cli.command()
+def model_info():
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
+@cli.command()
+def list():
+    """List various data from project. Products, masks, datasets, models"""
+    click.echo("NEEDS TO BE IMPLEMENTED!")
+
 
 
 if __name__ == '__main__':
-
-    if sys.version_info < (3, 5, 0):
-        sys.stderr.write("You need python 3.5 or later to run this script\n")
-        sys.exit(1)
-
-
-    args = cmdline_args()
-    print(args)
-    if hasattr(args, 'func'):
-        args.func(args)
-    else:
-        args.print_help()
-
+    cli()
