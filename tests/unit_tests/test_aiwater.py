fdaf1de25198c718c9169cde8344b1fd5b05b14b
jmherning <jmherning93@gmail.com>
2020-10-02 06:00:34 -0800

    removed temporary test

--- tests/unit_tests/test_aiwater.py
+++ tests/unit_tests/test_aiwater.py
@@ -2,12 +2,3 @@
 
 
 import pytest
-
-from argparse import Namespace
-from aiwater import hello
-
-def test_hello():
-    input_namespace = Namespace(name='jason', count=3)
-    input_str = hello(input_namespace)
-    assert input_str=="hello jason!hello jason!hello jason!"
-
