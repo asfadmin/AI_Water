4f9bd83787d109f0452c875042255cb2e8b384f8
jmherning <jmherning93@gmail.com>
2020-10-02 21:09:15 -0800

    added default directory paths to config file

--- src/config.py
+++ src/config.py
@@ -6,15 +6,41 @@
  Description:  Constants to be imported by other files.
 """
 
-
 import os
 import re
+
+# credit to https://github.com/treigerm/WaterNet/blob/master/waterNet/config.py
+
+from pathlib import Path
+
+# full Path to aiwater root
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+# Path configurations for data directory
+DATA_DIR = PROJECT_ROOT / "data"
+
+# Input data subdirectory path configs
+INPUT_DIR = DATA_DIR / "input"
+SENTINEL_DIR = INPUT_DIR / "sentinel1"
+SHAPEFILE_DIR = INPUT_DIR / "shape_files"
+
+# TODO: test directory?
+# Working data subdirectory path configs
+WORKING_DIR = DATA_DIR / "working"
+MODEL_WEIGHTS_DIR = WORKING_DIR / "models"
+TRAIN_DIR = WORKING_DIR / "training_data"
+TILES_DIR = TRAIN_DIR / "tiles"
+WATER_MASKS_DIR = TRAIN_DIR / "water_masks"
+LABELS_DIR = TRAIN_DIR / "labels_images"
+
+# Output data subdirectory path configs
+OUTPUT_DIR = DATA_DIR / "output"
+TENSORBOARD_DIR = OUTPUT_DIR / "tensorboard"
 
 PROJECT_DIR = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 DATASETS_DIR = os.path.join(PROJECT_DIR, 'datasets')
 MODELS_DIR = os.path.join(PROJECT_DIR, 'models')
 NETWORK_DEMS = 512
-
 
 VH_REGEX = re.compile(r'(.*)_([0-9]+).vh.tif')
 VV_REGEX = re.compile(r'(.*)_([0-9]+).vv.tif')
